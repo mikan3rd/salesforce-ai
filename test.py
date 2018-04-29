3c799221f25d052d73007e0857bb2f893106846e
mikan3rd <mikan.no.kikoudhi@gmail.com>
2018-04-29 18:53:00 +0900

    fix: settingsの追記

--- test.py
+++ test.py
@@ -11,10 +11,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import settings
+
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi(settings.YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.YOUR_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])

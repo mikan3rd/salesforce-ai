5c389f050a8e7c2edf59bc2961861e99a5f2cc8a
mikan3rd <mikan.no.kikoudhi@gmail.com>
2018-04-29 18:11:03 +0900

    fix: routeの修正2

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
 handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
